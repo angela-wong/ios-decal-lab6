2095ec9e1494fe2c0ad4dcbf283f091365efaa9e
angela-wong <angela.wong@berkeley.edu>
2017-04-09 04:59:22 -0700

    finished lab6

--- Login/LoginViewController.swift
+++ Login/LoginViewController.swift
@@ -17,20 +17,127 @@ class LoginViewController: UIViewController {
         static let invalidEmailMessage = "Please try again"
     }
 
+    var label: UILabel = {
+        let myLabel = UILabel(frame: CGRect(x: 0, y: 0,
+                                            width: UIScreen.main.bounds.width,
+                                            height: 300) )
+        
+        // configure the text of the button
+        myLabel.text = "Login View Controller"
+        myLabel.font = UIFont(name: "Avenir", size: 60)
+        myLabel.numberOfLines = 2
+        myLabel.textAlignment = .center
+        myLabel.textColor = .white
+        myLabel.backgroundColor = Constants.backgroundColor
+        return myLabel
+    }()
+    
+    var theView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .white
+        myView.layer.cornerRadius = 10
+        myView.layer.masksToBounds = true
+        return myView
+    }()
+    
+    var email: UITextField = {
+        let txt = UITextField()
+        txt.backgroundColor = .white
+        txt.text = "berkeley.edu account"
+        txt.font = UIFont(name: "avenir", size: 18)
+        txt.textAlignment = .left
+        txt.textColor = .gray
+        return txt
+    }()
+    
+    var password: UITextField = {
+        let txt = UITextField()
+        txt.backgroundColor = .white
+        txt.text = "Password"
+        txt.font = UIFont(name: "avenir", size: 18)
+        txt.textAlignment = .left
+        txt.textColor = .gray
+        return txt
+    }()
+    
+    var login: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 18)
+        button.titleLabel?.textAlignment = .center
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = Constants.backgroundColor
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        return button
+    }()
+    
+    
+    
     // TODO: instantiate the views needed for your project
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Constants.backgroundColor
-        
+        view.addSubview(label)
+        view.addSubview(theView)
+        theView.addSubview(email)
+        theView.addSubview(password)
+        theView.addSubview(login)
+        theView.translatesAutoresizingMaskIntoConstraints = false
+        email.translatesAutoresizingMaskIntoConstraints = false
+        password.translatesAutoresizingMaskIntoConstraints = false
+        login.translatesAutoresizingMaskIntoConstraints = false
+        let viewWidth: CGFloat = view.frame.width * 0.9
+        let myConstraints = [
+            
+            theView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            theView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            theView.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+                                             constant: view.frame.width*0.05),
+            //theView.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+               //                               constant: view.frame.width*0.05),
+            theView.widthAnchor.constraint(equalToConstant: viewWidth),
+            theView.heightAnchor.constraint(equalToConstant: 185),
+            email.centerXAnchor.constraint(equalTo: theView.centerXAnchor),
+            login.centerXAnchor.constraint(equalTo: theView.centerXAnchor),
+            password.centerXAnchor.constraint(equalTo: theView.centerXAnchor),
+            email.leadingAnchor.constraint(equalTo: theView.leadingAnchor,
+                                            constant: 15),
+            password.leadingAnchor.constraint(equalTo: theView.leadingAnchor,
+                                             constant: 15),
+            login.leadingAnchor.constraint(equalTo: theView.leadingAnchor,
+                                             constant: 70),
+            email.trailingAnchor.constraint(equalTo: theView.trailingAnchor,
+                                              constant: 15),
+            password.trailingAnchor.constraint(equalTo: theView.trailingAnchor,
+                                              constant: 15),
+            login.trailingAnchor.constraint(equalTo: theView.trailingAnchor,
+                                              constant: -70),
+            email.widthAnchor.constraint(equalToConstant: theView.frame.width*0.9),
+            password.widthAnchor.constraint(equalToConstant: theView.frame.width*0.9),
+            login.widthAnchor.constraint(equalToConstant: theView.frame.width-140),
+            email.heightAnchor.constraint(equalToConstant: 20),
+            password.heightAnchor.constraint(equalToConstant: 20),
+            login.heightAnchor.constraint(equalToConstant: 60),
+            email.topAnchor.constraint(equalTo: theView.topAnchor, constant: 20),
+            password.topAnchor.constraint(equalTo: email.bottomAnchor, constant: 20),
+            
+            login.topAnchor.constraint(equalTo: password.bottomAnchor, constant: 20),
+            login.bottomAnchor.constraint(equalTo: theView.bottomAnchor, constant: -20),
+            
+        ]
         // TODO: Add your views either as subviews of `view` or subviews of each other using `addSubview`
         
         // TODO: layout your views using frames or AutoLayout
+        NSLayoutConstraint.activate(myConstraints)
+        login.addTarget(self, action: #selector(checkLogin), for: .touchDown)
     }
     
     // TODO: create an IBAction for your login button
-    
-    
+    @IBAction func checkLogin(sender: UIButton) {
+        authenticateUser(username: email.text, password: password.text)
+    }
     
     
     
